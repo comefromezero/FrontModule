f54d0e232ca6d2e2b905e85b710000cfe2221630
lengye7 <zhangzhuo_ming@163.com>
2022-03-30 18:13:51 +0800

    更改目录结构与增加符号导入方式

--- ES6/main.js
+++ ES6/main.js
@@ -1,15 +1,18 @@
-import * as moduleA from "./moduleA.js";
+//import * as moduleA from "./moduleA.js";
+
+import {helloA} from "./moduleA.js"
 
 function main(){
     console.log("This is main!");
 }
 main();
-moduleA.helloA();
+//moduleA.helloA();
+helloA();
 
-import test from "./moduleB.js"
+import test, { helloB } from "./moduleB.js"
 
 console.log(test);
 
-import * as moduleC from "./moduleC.js"
+import * as moduleC from "./static/moduleC.js"
 
 moduleC.helloC();
